bf1e6174493105006360dc2d2929be1e7caedcd7
laibaa1209 <142867994+laibaa1209@users.noreply.github.com>
2023-10-03 08:56:54 +0500

    Update q01.c

--- lab/06/q01.c
+++ lab/06/q01.c
@@ -1,18 +1,22 @@
+//programmer:Laiba Zia
+//date:3rd october 2023
+//Question: Write a C Program that takes any number from the user and identifies if the number is a perfect number or not.
 #include<stdio.h>
-int main() {
+int main() { //start of main
 
-    int num, sum=0, i=1,a; 
-    printf("enter the number you want to check: ");
+    int num, sum=0, i=1,a;  //initialzing 
+    printf("enter the number you want to check: "); //asking the                           user to input the num to check
     scanf("%d",&num);
-    for( i=1;i<=num;i++) {
-        if(num%i==0) { 
-           a=i;
-           sum=sum+a;     
+    for( i=1;i<num;i++) { //begin of for loop
+        if(num%i==0) { //test condition to see if remainder is equal             0 or not
+           a=i; //giving value of i to variable a
+           sum=sum+a;    //taking the sum of factors 
      }
-  }
-      if(sum==num) { 
-         printf("it is a perfect number\n");
-      }
+  } //end of foor loop
+      if(sum==num) {  //test condition to check wether sum is equal to num or not
+         printf("it is a perfect number\n"); //output
+      } else {
      printf("it is not a perfect number");
+      } 
 return 0;
-}
+} //end main
