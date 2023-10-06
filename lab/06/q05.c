299937c719a3f6b79b40f577892adc6a1eb91463
laibaa1209 <142867994+laibaa1209@users.noreply.github.com>
2023-10-06 09:06:49 +0500

    Add files via upload

---
+++ lab/06/q05.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+int main() {
+   
+    for(int a=1;a<=4;a++) {
+        printf("0");
+        printf("\t\t");
+    } printf("\n");
+    for(int a=1;a<=2;a++) {
+        printf("\t\t");
+        printf("1");
+    } printf("\n");
+    for(int a=1;a<=4;a++) {
+        printf("2");
+        printf("\t\t");
+    } printf("\n");
+    for(int a=1;a<=2;a++) {
+        printf("\t\t");
+        printf("3");
+    } printf("\n");
+     for(int a=1;a<=4;a++) {
+        printf("4");
+        printf("\t\t");
+    } printf("\n");
+    for(int a=1;a<=2;a++) {
+        printf("\t\t");
+        printf("5");
+    } printf("\n");
+    for(int a=1;a<=4;a++) {
+        printf("6");
+        printf("\t\t");
+    } printf("\n");
+    
+    return 0;
+}
