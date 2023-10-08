b8d630c8ff33112c5797b9d4e1a42e208bab48ee
laibaa1209 <142867994+laibaa1209@users.noreply.github.com>
2023-10-08 23:31:07 +0500

    Update q02.c

--- lab/06/q02.c
+++ lab/06/q02.c
@@ -1,63 +1,29 @@
 //programmer:Laiba Zia
 //date:3rd october 2023
-//Question:Write a C program that produces the following output:
+//Question:Write a program that will generate the Fibonacci series up to 10000. Also find the sum of the generated Fibonacci numbers divisible by 3, 5 or 7 only.
+#include<stdio.h>
+int main() { //start of funtion main
 
-/*0  0  0  0
+    int a=1,b=1,c=0,sum=0;
+        printf("%d\t",a);
+    	printf("%d\t",b);
+    	for(c=0;c<10000;) {
+    		if(c%3==0 || c%5==0 || c%7==0) {
+    			sum=sum+c;
+			}
+    		a=b;
+    		b=c;
+    		c=a+b;
+    		
+    		if(c<10000) {	
+    		printf("%d\t",c);
+    }
+    		
+		}
+		
+		printf("\nsum:%d",sum);
+    	
+	
+	return 0;
 
-     1  1
-
-  2  2  2  2
-
-     3  3
-
-  4  4  4  4
-
-     5  5
-
-  6  6  6  6     */
-
-#include <stdio.h>
-
-int main() { //start of main
-     
-     //start of loops to print the above given output
-     
-    for(int a=1;a<=4;a++) {  
-        printf("0");
-        printf("\t\t");
-    } printf("\n");
-
-    for(int a=1;a<=2;a++) {
-        printf("\t\t");
-        printf("1");
-    } printf("\n");
-
-    for(int a=1;a<=4;a++) {
-        printf("2");
-        printf("\t\t");
-    } printf("\n");
-
-    for(int a=1;a<=2;a++) {
-        printf("\t\t");
-        printf("3");
-    } printf("\n");
-
-     for(int a=1;a<=4;a++) {
-        printf("4");
-        printf("\t\t");
-    } printf("\n");
-
-    for(int a=1;a<=2;a++) {
-        printf("\t\t");
-        printf("5");
-    } printf("\n");
-
-    for(int a=1;a<=4;a++) {
-        printf("6");
-        printf("\t\t");
-    } printf("\n");
-    
-    //end of loops
-    
-    return 0;
-}
+} //end of main
